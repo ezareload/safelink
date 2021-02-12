a64032ce20fbf7ce1dee1e7ec63bc47ef572ea8f
EZA RELOAD <m.imamtu@gmail.com>
2021-02-12 23:49:11 +0700

    Update safelink.js

--- safelink.js
+++ safelink.js
@@ -22,7 +22,6 @@ if(a_to_vh==false)
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="post-1.html"}
-if(x=="2"){xxx="post-2.html"}
 if(x=="3"){xxx="post-3.html"}
 if(x=="4"){xxx="post-4.html"}a_to_ve[i].href="https://directlink.elreload.top/2021/02/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
